12472e3c46340e6f805c436e327e8ceef1259f8c
SEKI-YUTA <zard.since1991@gmail.com>
2022-07-13 22:13:29 +0900

    add drawer menu and share image feature

--- app/src/main/java/com/example/galleryapp/ImageDetailActivity.java
+++ app/src/main/java/com/example/galleryapp/ImageDetailActivity.java
@@ -5,21 +5,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 public class ImageDetailActivity extends AppCompatActivity {
     private String imagePath;
     private ImageView img_large;
+    private ImageButton imgBtn_edit, imgBtn_share;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_detail);
 
         img_large = findViewById(R.id.img_large);
+        imgBtn_edit = findViewById(R.id.imgBtn_edit);
+        imgBtn_share = findViewById(R.id.imgBtn_share);
 
         Intent intent = getIntent();
         imagePath = intent.getStringExtra("path");
-
         img_large.setImageURI(Uri.parse(imagePath));
+
+        imgBtn_share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                shareIntent.setType("image/*");
+                shareIntent.putExtra(Intent.EXTRA_STREAM, imagePath);
+                startActivity(Intent.createChooser(shareIntent, "Share screen"));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/galleryapp/MainActivity.java
+++ app/src/main/java/com/example/galleryapp/MainActivity.java
@@ -1,31 +1,64 @@
 package com.example.galleryapp;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.Notification;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView recycler_images;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private ActionBarDrawerToggle drawerToggle;
     private List<String> paths = new ArrayList<>();
     private final int PERMISSION_REQCODE = 101;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.nav_view);
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open_drawer, R.string.close_drawer);
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navItem_about:
+                        Toast.makeText(MainActivity.this, "About item", Toast.LENGTH_SHORT).show();
+                        return true;
+                    default:
+                        Toast.makeText(MainActivity.this, "no matched", Toast.LENGTH_SHORT).show();
+                        return false;
+                }
+            }
+        });
+
         recycler_images = findViewById(R.id.recycler_images);
         recycler_images.setHasFixedSize(true);
         recycler_images.setLayoutManager(new GridLayoutManager(this, 3));
@@ -34,8 +67,10 @@ public class MainActivity extends AppCompatActivity {
         ImageAdapter adapter = new ImageAdapter(this, paths);
         recycler_images.setAdapter(adapter);
 
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_REQCODE);
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE
+        + Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_REQCODE);
         } else {
             appSetUP();
         }
@@ -67,5 +102,14 @@ public class MainActivity extends AppCompatActivity {
 
         ImageAdapter adapter = new ImageAdapter(this, paths);
         recycler_images.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
     }
 }
